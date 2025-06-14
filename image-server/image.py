28d410181d4f40c82c4c50ecb96bf96f01fc5b35
methu28 <77935285+methu28@users.noreply.github.com>
2025-06-14 17:59:51 -0400

    Update image.py

--- image-server/image.py
+++ image-server/image.py
@@ -60,10 +60,3 @@ def analyze():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
-
-
-
-    return jsonify({"caption": caption})
-
-if __name__ == '__main__':
-    app.run()
