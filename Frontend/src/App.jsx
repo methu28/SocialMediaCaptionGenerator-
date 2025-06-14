04000ef578334e0ccd89e00748c9fbcf5182d3f0
methu28 <77935285+methu28@users.noreply.github.com>
2025-06-14 16:13:41 -0400

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   const handleGenerateText = async () => {
     try {
-      const response = await fetch("https://socialmediacaptiongenerator-boot.onrender.com/api/generateText", {
+      const response = await fetch("https://socialmediacaptiongenerator-boot.onrender.com/api/generate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -46,7 +46,7 @@ function App() {
 
   const handleSuggestEnhancements = async () => {
     try {
-      const response = await fetch("https://socialmediacaptiongenerator-boot.onrender.com", {
+      const response = await fetch("https://socialmediacaptiongenerator-boot.onrender.com/api/generate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: generatedText }),
