a376534e61b0345834f0ccf755f468078b6e6a19
methu28 <77935285+methu28@users.noreply.github.com>
2025-06-13 23:57:56 -0400

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   const handleGenerateText = async () => {
     try {
-      const response = await fetch("https://socialmediacaptiongenerator-boot.onrender.com/api/generate", {
+      const response = await fetch("https://socialmediacaptiongenerator-boot.onrender.com/api/generateText", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
